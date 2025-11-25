760b62a80e633f7c892ebced91ad8f81578d1268
kiteswinoujscie <rafal@kiteswinoujscie.pl>
2025-11-25 07:19:15 +0100

    Update sensecap.py

--- sensecap.py
+++ sensecap.py
@@ -1,91 +1,65 @@
-
-#!/usr/bin/env python
-
-import minimalmodbus
 import time
-import syslog
 import weewx
 import weewx.drivers
+import minimalmodbus
+import logging
 
-DRIVER_NAME = "SenseCAP"
-DRIVER_VERSION = "0.3"
+log = logging.getLogger(__name__)
 
-def logmsg(dst, msg):
-    syslog.syslog(dst, f"{DRIVER_NAME}: {msg}")
+class SenseCAPS700Driver(weewx.drivers.AbstractDevice):
 
-def loginf(msg): logmsg(syslog.LOG_INFO, msg)
-def logdbg(msg): logmsg(syslog.LOG_DEBUG, msg)
-
-def loader(config_dict, _): return SenseCAPDriver(**config_dict[DRIVER_NAME])
-def confeditor_loader(): return SenseCAPConfEditor()
-
-class SenseCAPConfEditor(weewx.drivers.AbstractConfEditor):
-    @property
-    def default_stanza(self):
-        return f"""
-[{DRIVER_NAME}]
-    model = SenseCAP S700
-    port = /dev/ttyUSB0
-    poll_interval = 10
-    address = 1
-    driver = user.sensecap
-"""
-
-class SenseCAPDriver(weewx.drivers.AbstractDevice):
     def __init__(self, **stn_dict):
-        self.model = stn_dict.get("model", "SenseCAP S700")
-        self.port = stn_dict.get("port", "/dev/ttyUSB0")
-        self.poll_interval = int(stn_dict.get("poll_interval", 10))
-        self.address = int(stn_dict.get("address", 1))
-        self.instrument = minimalmodbus.Instrument(self.port, self.address)
+        self.port = stn_dict.get('port', '/dev/ttyUSB0')
+        self.slave = int(stn_dict.get('slave', 1))
+        self.poll_interval = float(stn_dict.get('poll_interval', 5))
+
+        self.instrument = minimalmodbus.Instrument(self.port, self.slave)
         self.instrument.serial.baudrate = 9600
         self.instrument.serial.timeout = 1
-        self.last_rain_total = None
-        loginf(f"Initialized {self.model} on {self.port}")
+        self.instrument.mode = minimalmodbus.MODE_RTU
+        self.instrument.clear_buffers_before_each_transaction = True
 
-    @property
-    def hardware_name(self):
-        return self.model
+    def read_int32(self, address):
+        regs = self.instrument.read_registers(address, 2, functioncode=4)
+        raw = (regs[0] << 16) | regs[1]
+        if raw & 0x80000000:
+            raw -= 0x100000000
+        return raw
 
-    def closePort(self):
-        self.instrument.serial.close()
+    def knots(self, ms):
+        return ms * 3.64384
 
     def genLoopPackets(self):
         while True:
             try:
-                data = self.read_sensor_data()
-                packet = {
-                    'dateTime': int(time.time()),
-                    'usUnits': weewx.METRIC,
-                    **data
-                }
+                packet = {}
+                packet['dateTime'] = int(time.time())
+                packet['usUnits'] = weewx.METRIC
+
+                packet['windSpeed'] = self.knots(self.read_int32(0x0012) / 1000)
+                packet['windGust'] = self.knots(self.read_int32(0x0010) / 1000)
+                packet['windDir'] = self.read_int32(0x000C) / 1000
+
+                packet['outTemp'] = self.read_int32(0x0000) / 1000
+                packet['outHumidity'] = self.read_int32(0x0002) / 1000
+                packet['barometer'] = self.read_int32(0x0004) / 1000
+
+                packet['rain'] = self.read_int32(0x0014) / 100000000
+                packet['rainRate'] = self.read_int32(0x0018) / 100000000
+
+                packet['radiation'] = self.read_int32(0x0006) / 100000
+
                 yield packet
-                time.sleep(self.poll_interval)
+
             except Exception as e:
-                logmsg(syslog.LOG_ERR, f"Error: {e}")
-                time.sleep(self.poll_interval)
+                log.error(f"Modbus error: {e}")
 
-    def read_sensor_data(self):
-        r = self.instrument.read_registers
-        def read_int32(addr):
-            h, l = r(addr, 2)
-            return (h << 16) + l
+            time.sleep(self.poll_interval)
 
-        curr_rain_total = read_int32(0x0014) / 1000.0  # mm
-        rain_delta = 0.0
-        if self.last_rain_total is not None:
-            rain_delta = max(0.0, curr_rain_total - self.last_rain_total)
-        self.last_rain_total = curr_rain_total
+    @property
+    def hardware_name(self):
+        return "SenseCAP S700"
 
-        data = {
-            'outTemp': read_int32(0x0000) / 1000.0,         # °C
-            'outHumidity': read_int32(0x0002) / 1000.0,     # %RH
-            'pressure': read_int32(0x0004) / 100000.0,        # hPa
-            'radiation': read_int32(0x0006) / 1000.0,       # lux
-            'windDir': read_int32(0x000C) / 1000.0,         # degrees
-            'windSpeed': read_int32(0x0012) / 1000.0,       # m/s
-            'windGust': read_int32(0x0010) / 1000.0,        # m/s
-            'rainRate': read_int32(0x0018) / 1000.0,        # mm/min
-            'rain': rain_delta                             # mm od ostatniego odczytu
-        }
-        return data
+
+def loader(config_dict, _):
+    return SenseCAPS700Driver(**config_dict['SenseCAPS700'])
